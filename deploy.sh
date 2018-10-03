9cdb8bf01854fc3e611fdfdb1ae5b1c2221f7fbb
wiertar <wiertar@ferris.edu>
2018-10-03 17:28:24 -0400

    Added CD

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#Set up git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://github.com/wiertar/wiertar.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
