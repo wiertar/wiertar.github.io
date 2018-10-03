102cb212fb89323fe22d3cefa641a0482a8bf217
wiertar <wiertar@ferris.edu>
2018-10-03 17:54:24 -0400

    Fixed CD

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/wiertar/wiertar.github.io.git master
+git remote add origin https://github.com/wiertar/wiertar.github.io.git
+git push -f https://${GITHUB_TOKEN}@github.com/wiertar/wiertar.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
