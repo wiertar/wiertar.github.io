3cf43a35ea1a49c768a1279be6478ada063b92c0
wiertar <wiertar@ferris.edu>
2018-10-05 07:17:28 -0400

    Working on heading

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="outer">
+    <div class="inner">
+      <div class="heading">
+        <h1>Richard Wiertalla</h1>
+        <h3>Developer Portfolio</h3>
+        <ul class="heading__list">
+          <li class="heading__list--item"><a class="heading__link" href="#">Github</a></li>
+          <li class="heading__list--item"><a class="heading__link" href="#">Contact Me</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'app',
   components: {
-    HelloWorld,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css?family=Permanent+Marker');
+
+*, *::before, *::after {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-size: 62.5%;
+  font-family: sans-serif;
+  font-family: 'Permanent Marker', cursive;
+}
+
+.outer {
+  display: flex;
+  flex-direction: column;
+  
+}
+
+.inner {
+  display: flex;
+  justify-content: center;
+}
+
+.heading {
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  &__list {
+    &--item {
+
+    }
+  }
+
+  &__link {
+    
+  }
+}
+
+#app {
+  height: 100vh;
+  flex-direction: column;
+  h1 {
+    color: black;
+    font-size: 3.5rem;
+  }
+  h3 {
+    color: cornflowerblue;
+    font-size: 2.5rem;
+  }
 }
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,20 +1,20 @@
-import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+// import { expect } from 'chai';
+// import { shallowMount } from '@vue/test-utils';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).to.include(msg);
-  });
-});
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message';
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg },
+//     });
+//     expect(wrapper.text()).to.include(msg);
+//   });
+// });
 
-describe('HelloWorld.vue', () => {
-  it('should have a prop called name', () => {
-    const wrapper = shallowMount(HelloWorld);
-    expect(wrapper.name()).to.equal('HelloWorld');
-  });
-});
+// describe('HelloWorld.vue', () => {
+//   it('should have a prop called name', () => {
+//     const wrapper = shallowMount(HelloWorld);
+//     expect(wrapper.name()).to.equal('HelloWorld');
+//   });
+// });
